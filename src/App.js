8ecb111bb2165cdf59a5093517cf930f5c29bbf3
-THE-LAST-SAMURAI- <connor813@outlook.com>
2021-10-22 13:04:59 -0500

    added image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from './components/Header'
 import Footer from "./components/Footer";
+import Image from "./components/Image";
+
 
 function App() {
 
@@ -8,6 +10,7 @@ function App() {
   return (
     <div>
     <Header />
+    <Image />
     <Footer />
    </div>
   );
---
+++ src/components/Image.js
@@ -0,0 +1,18 @@
+import React from "react";
+import photo_tagging_image from '../images/photo-tagging-image.jpeg'
+
+
+function Image() {
+
+
+    return (
+        <div>
+            <img className="photo_tagging_image" alt="photo_tagging_image" src={photo_tagging_image} />
+        </div>
+    );
+}
+
+
+export default Image
+
+
