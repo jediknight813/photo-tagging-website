ea6b4688b4f66c823ddbd0a5a99c79736e99c57a
-THE-LAST-SAMURAI- <connor813@outlook.com>
2021-10-28 13:30:18 -0500

    added leaderboard with name and time

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -2,34 +2,64 @@ import React, { useState } from "react";
 import { return_user_data } from './firebase'
 
 
+var user_list = []
+var leader_board_classname = 'leader_board_name_background_hidden'
+
+
+var hide_leader_board = true
+
 function Leaderboard() {
-    const [users, updateUsers] = useState(return_user_data)
+    user_list.splice(0, user_list.length)
+    const [users, updateUsers] = useState([])
+    const [, updateState] = React.useState();
+    const forceUpdate = React.useCallback(() => updateState({}), []);
+
 
     function update_user_list() {
-        updateUsers(return_user_data)
-        updateUsers(return_user_data)
+        updateUsers(return_user_data())
+        if (hide_leader_board === true) {
+            hide_leader_board = false
+            leader_board_classname = 'leader_board_name_background'
+            DisplayUsersData()
+            forceUpdate()
+        }
+
+        else {
+            hide_leader_board = true
+            leader_board_classname = 'leader_board_name_background_hidden'
+            forceUpdate()
+        }
+        
+
+        ///Object.entries(users).forEach((entry) => {
+         //   console.log(entry[1]['username'], entry[1]['time']);
+         // });
+
 
         DisplayUsersData()
-        Object.entries(users).forEach((entry) => {
-            console.log(entry[1]['username'], entry[1]['time']);
-          });
-    }
-
-
+    }   
 
 
     function  DisplayUsersData() {
+        console.log(users)
+        var leaderboard_list = []
 
+        Object.entries(users).forEach((entry) => {
+             leaderboard_list.push([entry[1]['username'], entry[1]['time']]);
+             });
+
+        console.log(leaderboard_list +" leaderboard list")
 
         return (
-            <div className="leader_board_name_background">
-
-            {Object.entries(users).forEach((entry) => {
-            <h1> {entry[1]['username'] + entry[1]['time']} </h1>
-
-          })}
+            <div className={leader_board_classname}>
 
 
+                <h1 className="leaderboard_user_text"> Name----Time </h1>
+
+                    {leaderboard_list.map(Element =>
+                            <h2 className="leaderboard_user_text" key={Math.random()}> {Element[0]+ ":   " + Element[1]} </h2>    
+                        
+                    )}
 
             </div>    
 
